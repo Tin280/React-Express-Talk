4e9c787b9862cd4a56daa7cf78fa41c1dddc65de
Tin Nguyen <nguyentritin211@gmail.com>
2023-03-17 00:37:21 +0200

    update

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -1,20 +1,26 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 
 const App = () => {
   const [scanData, setScanData] = useState([]);
-
   useEffect(() => {
-    fetch('/api/scan')
-      .then(res => res.json())
-      .then(data => setScanData(data))
-      .catch(error => console.log(error));
+    const intervalID = setInterval(() => {
+      console.log("From interval");
+      fetch("/api/scan")
+        .then((res) => res.json())
+        .then((data) => setScanData(data))
+        .catch((error) => console.log(error));
+    }, 1000);
+    console.log("FROM USE EFFECT");
+    return () => {
+      clearInterval(intervalID);
+    };
   }, []);
 
   return (
     <div>
       <h1>Scan list:</h1>
       <ul>
-        {scanData.map(scan => (
+        {scanData.map((scan) => (
           <li key={scan.name}>
             {scan.name} - {scan.price}
           </li>
